c15af9dce3de7cf368b96f4982b8074f964b0155
RYU <ryuryuryu339@likelion.org>
2021-07-09 12:59:12 +0900

    21.07.09 New Project commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,11 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import NewModel
+
 
 def hello_world(request):
     if request.method == 'POST':
+
+        temp = request.POST.get('typing')
+
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+
         return render(request, 'accountapp/gogo.html',
-                      context={'text' : 'HELLO'})
+                      context={'new_model':new_model})
     else:
         return render(request, 'accountapp/gogo.html',
-                      context={'text' : 'HI THERE'})
+                      context={'text':'HI THERE'})
